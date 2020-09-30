b79aae792e764057ec7e2c884cee5e78ef8f9aef
alethkit <git@alethk.it>
2020-09-30 12:18:32 +0100

    added basic runtime

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,18 @@ extern {
 
 #[no_mangle]
 pub extern "C" fn _wumbo() -> ! {
+    unsafe {
+        asm!("ldr {1}, {0}", "mov sp, {1}", sym _wumbo, out(reg) _);
+        zero_memory_section(__bss_start as *mut usize, __bss_end as *mut usize);
+    }
     panic!()
+}
+
+unsafe fn zero_memory_section(mut start_addr: *mut usize, end_addr: *mut usize) {
+    while start_addr < end_addr {
+        start_addr.write_volatile(0);
+        start_addr = start_addr.offset(1);
+    }
 }
 
 #[panic_handler]
